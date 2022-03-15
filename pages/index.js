22139d0c23e2230c5043d6e231614e2cb901de71
HyoSean <kimhyosun@gmail.com>
2022-03-15 19:49:35 +0900

    메인 리스트

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,24 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   const API_URL = 'https://dog.ceo/api/breeds/';
   const [list, setList] = useState([]);
+  const [imgUrl, setImgUrl] = useState([]);
 
   const getData = async () => {
-    const response = await axios.get(`${API_URL}list/all`);
-    const list = Object.keys(response.data.message);
-    setList(list);
+    try {
+      const response = await axios.get(`${API_URL}list/all`);
+      const list = Object.keys(response.data.message);
+      setList(list);
+
+      Promise.all(list.map(async (dogs) => await axios.get(`https://dog.ceo/api/breed/${dogs}/images`))).then(
+        (values) => {
+          const imgsres = values.map((cur) => cur.data.message[0]);
+          console.log(imgsres);
+          setImgUrl(imgsres);
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => getData(), []);
@@ -20,10 +33,10 @@ export default function Home() {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <title>There is no bad dog in the world</title>
       </Head>
-      <ItemList list={list} API_URL={API_URL} />
+      <ItemList list={list} imgUrl={imgUrl} />
     </>
   );
 }
--- src/component/ItemList.js
+++ src/component/ItemList.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import Link from 'next/link';
+import React, { useEffect } from 'react';
+// import Isotope from 'isotope-layout';
 
-const ItemList = ({ list, API_URL }) => {
+const ItemList = ({ list, imgUrl }) => {
+  // useEffect(() => {
+  //   //isotope layout
+  //   const grid = document.querySelector('.dogs_list');
+  //   const iso = new Isotope(grid, {
+  //     // options...
+  //     itemSelector: '.dogs_item',
+  //     masonry: {
+  //       columnWidth: 200,
+  //     },
+  //   });
+  // }, []);
   return (
     <>
-      <ul className="search_list">
+      <ul className="dogs_list">
         {list.map((cur, idx) => (
-          <li key={idx}>
-            <span>{cur}</span>
-            {/* <img src={imgUrl[idx]} alt={cur} /> */}
-          </li>
+          <Link href={`/view/${idx}`}>
+            <li className="dogs_item" key={`${idx}_${new Date().toString()}`}>
+              <span>{cur}</span>
+              <img src={imgUrl[idx]} alt={cur} />
+            </li>
+          </Link>
         ))}
       </ul>
     </>
