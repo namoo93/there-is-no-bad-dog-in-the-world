b17261dbb35377f0e4794165875b22d46dc26ba1
HyoSean <kimhyosun@gmail.com>
2022-03-22 18:47:40 +0900

    404 페이지

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div></div>;
+  return <div className="footer">here comes puppy world</div>;
 };
 
 export default Footer;
--- src/component/Gnb.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Gnb = () => {
-  return <div></div>;
-};
-
-export default Gnb;
